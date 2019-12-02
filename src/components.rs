70a32a22139d1e686cbd724d75f63da6d5795fa3
Follpvosten <wolfi@karpador.xyz>
2019-12-02 14:28:20 +0100

    Move over to using cfg-if

--- src/components.rs
+++ src/components.rs
@@ -1,44 +1,55 @@
-#[cfg(feature = "button")]
-pub mod button;
-#[cfg(feature = "button")]
-pub use button::Button;
+use cfg_if::cfg_if;
 
-#[cfg(feature = "icon-button")]
-pub mod icon_button;
-#[cfg(feature = "icon-button")]
-pub use icon_button::IconButton;
+cfg_if! {
+    if #[cfg(feature = "button")] {
+        pub mod button;
+        pub use button::Button;
+    }
+}
 
-#[cfg(feature = "fab")]
-pub mod fab;
-#[cfg(feature = "fab")]
-pub use fab::FAB;
+cfg_if! {
+    if #[cfg(feature = "icon-button")] {
+        pub mod icon_button;
+        pub use icon_button::IconButton;
+    }
+}
 
-#[cfg(feature = "text-field")]
-pub mod text_field;
-#[cfg(feature = "text-field")]
-pub use text_field::HelperLine as TextFieldHelperLine;
-#[cfg(feature = "text-field")]
-pub use text_field::TextField;
+cfg_if! {
+    if #[cfg(feature = "fab")] {
+        pub mod fab;
+        pub use fab::FAB;
+    }
+}
 
-#[cfg(feature = "menu")]
-pub mod menu;
-#[cfg(feature = "menu")]
-pub use menu::Item as MenuItem;
-#[cfg(feature = "menu")]
-pub use menu::Menu;
+cfg_if! {
+    if #[cfg(feature = "text-field")] {
+        pub mod text_field;
+        pub use text_field::HelperLine as TextFieldHelperLine;
+        pub use text_field::TextField;
+    }
+}
 
-#[cfg(feature = "drawer")]
-pub mod drawer;
-#[cfg(feature = "drawer")]
-pub use drawer::Content as DrawerContent;
-#[cfg(feature = "drawer")]
-pub use drawer::Drawer;
-#[cfg(feature = "drawer")]
-pub use drawer::Header as DrawerHeader;
+cfg_if! {
+    if #[cfg(feature = "menu")] {
+        pub mod menu;
+        pub use menu::Item as MenuItem;
+        pub use menu::Menu;
+    }
+}
 
-#[cfg(feature = "top-app-bar")]
-pub mod top_app_bar;
-#[cfg(feature = "top-app-bar")]
-pub use top_app_bar::Section as TopAppBarSection;
-#[cfg(feature = "top-app-bar")]
-pub use top_app_bar::TopAppBar;
+cfg_if! {
+    if #[cfg(feature = "drawer")] {
+        pub mod drawer;
+        pub use drawer::Content as DrawerContent;
+        pub use drawer::Drawer;
+        pub use drawer::Header as DrawerHeader;
+    }
+}
+
+cfg_if! {
+    if #[cfg(feature = "top-app-bar")] {
+        pub mod top_app_bar;
+        pub use top_app_bar::Section as TopAppBarSection;
+        pub use top_app_bar::TopAppBar;
+    }
+}
