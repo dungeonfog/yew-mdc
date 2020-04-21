09975dd5b851027132e184d25b586f526a1069c5
Follpvosten <wolfi@karpador.xyz>
2020-04-21 15:41:46 +0200

    Add onclick event to image list items

--- src/components/image_list/item.rs
+++ src/components/image_list/item.rs
@@ -7,6 +7,7 @@ pub use supporting::Supporting;
 
 pub struct Item {
     props: Props,
+    link: ComponentLink<Self>,
 }
 
 #[derive(Properties, Clone, PartialEq)]
@@ -16,14 +17,20 @@ pub struct Props {
     pub id: String,
     #[prop_or_default]
     pub classes: String,
+    #[prop_or_else(Callback::noop)]
+    pub onclick: Callback<MouseEvent>,
+}
+
+pub enum Msg {
+    Clicked(MouseEvent),
 }
 
 impl Component for Item {
-    type Message = ();
+    type Message = Msg;
     type Properties = Props;
 
-    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self { props }
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self { props, link }
     }
 
     fn change(&mut self, props: Props) -> ShouldRender {
@@ -35,13 +42,21 @@ impl Component for Item {
         }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Clicked(ev) => {
+                self.props.onclick.emit(ev);
+            }
+        }
         false
     }
 
     fn view(&self) -> Html {
         html! {
-            <li id=&self.props.id class=format!("mdc-image-list__item {}", self.props.classes)>
+            <li id=&self.props.id
+                class=format!("mdc-image-list__item {}", self.props.classes)
+                onclick=self.link.callback(Msg::Clicked)
+                >
                 { self.props.children.render() }
             </li>
         }
